1495af0ca8118cd341351d55186050deaa36e7db
tomoeoo <67987151+tomoeoo@users.noreply.github.com>
2026-05-08 14:18:17 +0800

    Add build.gradle.kts for Android application setup

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.lanshare"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.lanshare"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("org.webrtc:google-webrtc:1.0.32006")
+    implementation("org.jmdns:jmdns:3.5.8")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}
