4178bc9eaa29cc2b57b2a2777a13d7d3d383607a
tomoeoo <67987151+tomoeoo@users.noreply.github.com>
2026-05-08 15:09:09 +0800

    Add JitPack repository to settings.gradle.kts
    Add JitPack repository to project settings.

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -10,6 +10,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }   // 新增这一行
     }
 }
 rootProject.name = "LanShare"
